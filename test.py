ad2022bbb48114e019357f285b74cb2a4bfb2663
nitin.sharma <nitin.sharma@Nitin-Sharmas-MacBook-Pro.local>
2020-06-01 15:50:52 +0530

    Test

---
+++ test.py
@@ -0,0 +1,7 @@
+# I am a comment...
+x = 1   # the rest of the line is a comment
+        # ... and this is a 3rd comment
+text = "# But this isn't a comment because it's a string literal and in quotes."
+print (text)
+# This is a comment
+# that crosses multiple lines
